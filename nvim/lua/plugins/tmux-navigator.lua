28cfef0c0bdabc1aec199f67de51389ee51c9018
David Sinclair <david@codebreeze.se>
2024-01-31 08:46:19 +0100

    Add missing keybindings for tmux-navigator

--- nvim/lua/plugins/tmux-navigator.lua
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -1,4 +1,10 @@
 return {
   "christoomey/vim-tmux-navigator",
   lazy = false,
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Go to the left pane" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Go to the lower pane" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Go to the upper pane" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Go to the right pane" },
+  },
 }
