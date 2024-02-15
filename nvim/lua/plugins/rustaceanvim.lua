c1a090c576714028dfbe8c663bad1a6da24f07cc
David Sinclair <david@codebreeze.se>
2024-02-15 23:27:26 +0100

    Remove inlay hint config

--- nvim/lua/plugins/rustaceanvim.lua
+++ nvim/lua/plugins/rustaceanvim.lua
@@ -3,13 +3,4 @@ return {
   version = "^4",
   ft = { "rust" },
   dependencies = "neovim/nvim-lspconfig",
-  config = function()
-    vim.g.rustaceanvim = {
-      server = {
-        on_attach = function(_client, bufnr)
-          vim.lsp.inlay_hint(bufnr, true)
-        end,
-      },
-    }
-  end,
 }
